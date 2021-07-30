43bedd56ef2dfb235caa0c65bfd33eb1e8ef5ac5
r-jnak <jn.0124@gmail.com>
2021-07-30 12:52:15 +0900

    flask_restful導入

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,8 +1,16 @@
 from flask import Blueprint
+from flask_restful import Resource, Api 
 
 app = Blueprint('user', __name__)
+api = Api(app)
 
+class User(Resource):
+    def get(self, id):
+        return {"id": id}
 
-@app.route('/user')
-def index():
-    return "user"
+class UserLists(Resource):
+    def get(self):
+        return {"id": "all_user"}
+
+api.add_resource(UserLists, '/users')
+api.add_resource(User, '/users/<string:id>')
